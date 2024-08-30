0d9d5507b2fc275748f240f7c4a3b43c895a6e47
Nyded <mamadou.balde@gegmgroup.com>
2024-08-30 00:29:33 +0000

    [VIT-21] language en/fr support

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -2,8 +2,11 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NAVS from '../constants/nav'
+import { useTranslation } from 'react-i18next'
+import LanguageSwitcher from './LanguageSwitcher'
 
 const NavBar = () => {
+    const { t } = useTranslation()
     return (
         <Navbar expand='lg' fixed='top'>
             <Container>
@@ -13,9 +16,11 @@ const NavBar = () => {
                     <Nav id='tqs-navbar' className='justify-content-end'>
                         {NAVS.map(({ key, menuanchor, title }) => (
                             <Nav.Link key={key} data-menuanchor={menuanchor} href={key}>
-                                {title}
+                                {t(title)}
                             </Nav.Link>
                         ))}
+
+                        <LanguageSwitcher />
                     </Nav>
                 </Navbar.Collapse>
             </Container>
